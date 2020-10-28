7d7b06cce91e6307178564c42403e14eb4b88f0d
Alipari12 <alipari12@hotmail.com>
2020-10-28 15:38:02 -0400

    Create ListTasks class

--- app/src/main/java/com/example/mycalendar/MainActivity.java
+++ app/src/main/java/com/example/mycalendar/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView Date;
     private Button btnToCalendar;
-
+ //Test 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +31,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.nav_activity_main);
 
         Date = (TextView) findViewById(R.id.Date);
-        btnToCalendar = (Button) findViewById(R.id.btnToCalendar);
+        //btnToCalendar = (Button) findViewById(R.id.btnToCalendar);
 
 
-        Intent dateIntent =getIntent();
-        String Selected_Date= dateIntent.getStringExtra("date");
-        Date.setText(Selected_Date);
+//        Intent dateIntent =getIntent();
+//        String Selected_Date= dateIntent.getStringExtra("date");
+//        Date.setText(Selected_Date);
 
 
-        btnToCalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,CalendarPage.class);
-                startActivity(intent);
-            }
-        });
+//        btnToCalendar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,CalendarPage.class);
+//                startActivity(intent);
+//            }
+//        });
 
+
+        // This is the code for the swipe menu
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //Just a toast for Home
                 if(item.getItemId()==R.id.nav_home){
                     Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
                 }
+                //Makes it to where if you hit the calendar button it goes to the calendar
                 if(item.getItemId()==R.id.nav_calendar){
                     Intent intent = new Intent(MainActivity.this,CalendarPage.class);
                     startActivity(intent);
                 }
+                //Default code needed to run
                 DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
                 drawerLayout.closeDrawer(GravityCompat.START);
                 return true;
